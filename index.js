8e88d6a1e9040c61e6065d58ac42aab748aaf49b
Ron Snyder <ron@snyderjr.com>
2024-08-07 14:24:44 -0400

    test

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 console.log(window.config)
 
 console.log('add stylesheet')
-let stylesheet = document.createElement('link')
-stylesheet.rel = 'stylesheet'
-stylesheet.href = 'https://cdn.jsdelivr.net/npm/juncture-digital/css/index.css'
-document.head.appendChild(stylesheet)
 
-console.log('add web component script')
-let script = document.createElement('script')
-script.src = 'https://cdn.jsdelivr.net/npm/juncture-digital/js/index.js'
-script.type = 'module'
-document.head.appendChild(script)
+function addStylesheet() {
+  console.log('add stylesheet')
+  let stylesheet = document.createElement('link')
+  stylesheet.rel = 'stylesheet'
+  stylesheet.href = 'https://cdn.jsdelivr.net/npm/juncture-digital/css/index.css'
+  document.head.appendChild(stylesheet)
+}
 
-(function() {
-    console.log('init')
-    let orig = document.querySelector('article')
+function addWc() {
+  console.log('add web component script')
+  let script = document.createElement('script')
+  script.src = 'https://cdn.jsdelivr.net/npm/juncture-digital/js/index.js'
+  script.type = 'module'
+  document.head.appendChild(script)
+  }
 
-    let article = new DOMParser().parseFromString(window.config.content, 'text/html').querySelector('body')
-    article.querySelectorAll('code').forEach(codeEl => {
-        let junctureEl = document.createElement('ve-image')
-        junctureEl.setAttribute('src', 'wc:Sunflower_sky_backdrop.jpg')
-        junctureEl.classList.add('right')
-        codeEl.replaceWith(junctureEl)
-    })
-    console.log(article)
-    
-    orig.replaceWith(article)
- })()
+function docReady(fn) {
+  if (document.readyState === 'complete' || document.readyState === 'interactive') setTimeout(fn, 1)
+  else document.addEventListener('DOMContentLoaded', fn)
+}
+
+addStylesheet()
+addWc()
+
+docReady(function() {
+  console.log('init')
+  
+  let orig = document.querySelector('article')
+  
+  let article = new DOMParser().parseFromString(window.config.content, 'text/html').querySelector('body')
+  article.querySelectorAll('code').forEach(codeEl => {
+      let junctureEl = document.createElement('ve-image')
+      junctureEl.setAttribute('src', 'wc:Sunflower_sky_backdrop.jpg')
+      junctureEl.classList.add('right')
+      codeEl.replaceWith(junctureEl)
+  })
+  console.log(article)
+  
+  orig.replaceWith(article)
+})
 
  console.log('js loaded')
