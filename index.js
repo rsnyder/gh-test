f438575c8b4a706792dd7d34aef107a459116108
Ron Snyder <ron@snyderjr.com>
2024-08-07 14:16:19 -0400

    test

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 console.log(window.config)
 
+console.log('add stylesheet')
 let stylesheet = document.createElement('link')
 stylesheet.rel = 'stylesheet'
 stylesheet.href = 'https://cdn.jsdelivr.net/npm/juncture-digital/css/index.css'
 document.head.appendChild(stylesheet)
 
+console.log('add web component script')
 let script = document.createElement('script')
 script.src = 'https://cdn.jsdelivr.net/npm/juncture-digital/js/index.js'
 script.type = 'module'
 document.head.appendChild(script)
 
-let orig = document.querySelector('article')
+(function() {
+    console.log('init')
+    let orig = document.querySelector('article')
 
-let article = new DOMParser().parseFromString(window.config.content, 'text/html').querySelector('body')
-article.querySelectorAll('code').forEach(codeEl => {
-    let junctureEl = document.createElement('ve-image')
-    junctureEl.setAttribute('src', 'wc:Sunflower_sky_backdrop.jpg')
-    junctureEl.classList.add('right')
-    codeEl.replaceWith(junctureEl)
-})
-console.log(article)
+    let article = new DOMParser().parseFromString(window.config.content, 'text/html').querySelector('body')
+    article.querySelectorAll('code').forEach(codeEl => {
+        let junctureEl = document.createElement('ve-image')
+        junctureEl.setAttribute('src', 'wc:Sunflower_sky_backdrop.jpg')
+        junctureEl.classList.add('right')
+        codeEl.replaceWith(junctureEl)
+    })
+    console.log(article)
+    
+    orig.replaceWith(article)
+ })()
 
-orig.replaceWith(article)
+ console.log('js loaded')
